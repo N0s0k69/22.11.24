0202fe774104ffb06ecd2fae33666bd8d002510b
N0s0k69 <halyvasires@gmail.com>
2024-11-27 08:15:03 +0000

    3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(res)=>{
-    res.json(
-        {
-            message:"Hello Worda",
-        }
-    );
-}
-);
-app.listen(5050, ()=> console.log("Listening on http://localhost:5050"));
+app.get("/", (res) => {
+  res.json({
+    message: "Hello Worda",
+  });
+});
+app.listen(5050, () => console.log("Listening on http://localhost:5050"));
